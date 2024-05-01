4b4ce981342cfff4dca8140173e627362b398216
saramerlo <160161525+saramerlo@users.noreply.github.com>
2024-05-01 12:51:29 +0200

    aggiunto javascropt

---
+++ script.js
@@ -0,0 +1,17 @@
+//chiudi menù mobile
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Aggiungi un gestore di eventi per il clic su tutto il documento
+    document.addEventListener("click", function (event) {
+        // Verifica se il clic è avvenuto all'interno della navbar
+        let isClickInsideNavbar = document.querySelector('.navbar').contains(event.target);
+        // Verifica se il clic è avvenuto all'interno del menu mobile
+        let isClickInsideMenu = document.getElementById('navbarSupportedContent').contains(event.target);
+        // Se il clic è avvenuto al di fuori della navbar e del menu mobile, chiudi il menu
+        if (!isClickInsideNavbar && !isClickInsideMenu) {
+            document.getElementById('navbarSupportedContent').classList.remove('show');
+        }
+    });
+});
+
+
